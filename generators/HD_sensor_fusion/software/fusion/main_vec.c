7ec149a8491db8c73ff62fb085ab2e3b70ee6d76
Alisha Menon <allymenon@berkeley.edu>
2020-05-12 15:04:08 -0700

    debugging shift

--- generators/HD_sensor_fusion/software/fusion/main_vec.c
+++ generators/HD_sensor_fusion/software/fusion/main_vec.c
@@ -70,10 +70,11 @@ int main(){
             //Here the hypervector q[0] is shifted by 64 bits as permutation (no circularity),
 			//before performing the componentwise XOR operation with the new query (q[z]).
             //Much more hardware optimal!
-            void * xor_ngram_addr;
-            asm volatile ("vsetcfg %0" : : "r" (VCFG(N, 0, 0, 1)));
-            //uint64_t one = 0x1ULL;
-            //asm volatile ("vmcs vs1, %0" : : "r" (one))
+            //void * xor_ngram_addr;
+            void * ngram1_bind_addr;
+            asm volatile ("vsetcfg %0" : : "r" (VCFG(N+1, 0, 0, 1)));
+            uint64_t one = 0x1ULL;
+            asm volatile ("vmcs vs1, %0" : : "r" (one));
             //Vectorize this block
             //for(int b = bit_dim; b >= 0; ){
             //    int consumed; 
@@ -92,24 +93,24 @@ int main(){
 
             for(int b = 0; b < bit_dim+1; ){
                 asm volatile ("vsetvl %0, %1" : "=r" (consumed) : "r" (bit_dim+1-b));
-                for (int x = 0; x < consumed; x++){
+                //for (int x = 0; x < consumed; x++){
                     //if (x == 0) {
                     //    xor[b+x] = 0ULL;
                     //}
                     //else {
-                        xor[b+x] = q[0][b+x-1];
+                    //    xor[b+x] = q[0][b+x-1];
                     //}
                     //printf("%d\n", x);
-                }
+                //}
                 //for (int x = 0; x < consumed; x++){
                 //    q[0][b+x] = q[z][b+x] ^ xor[b+x];
                 //}
                 //q[0][b] = q[z][b] ^ xor[b];
                 asm volatile ("vmca va0, %0" : : "r" (&q[0][b]));
                 asm volatile ("vmca va1, %0" : : "r" (&q[z][b]));
-                asm volatile ("vmca va2, %0" : : "r" (&xor[b]));
-                asm volatile ("la %0, xor_ngram_v" : "=r" (xor_ngram_addr));
-                asm volatile ("vf 0(%0)" : : "r" (xor_ngram_addr));
+                //asm volatile ("vmca va2, %0" : : "r" (&xor[b]));
+                asm volatile ("la %0, ngram1_bind_v" : "=r" (ngram1_bind_addr));
+                asm volatile ("vf 0(%0)" : : "r" (ngram1_bind_addr));
                 //printf("%d\n", b);
                 //printf("%d\n", consumed);
                 b += consumed;
