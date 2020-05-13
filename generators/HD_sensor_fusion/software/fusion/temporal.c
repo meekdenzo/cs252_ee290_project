cc81e5541382e7ca70a600d8caf8edeb4703e929
Alisha Menon <allymenon@berkeley.edu>
2020-05-12 20:43:22 -0700

    bugs

--- generators/HD_sensor_fusion/software/fusion/temporal.c
+++ generators/HD_sensor_fusion/software/fusion/temporal.c
@@ -4,6 +4,7 @@
 #include "util.h"
 #include "fusion_funcs.h"
 #include "init.h"
+#include "vec-util.h"
 //the data.h and mems_<early/late>.h file can be created directly in MATLAB (after the simulation)
 //using the function "data_file_creator.m"
 #include "data.h"
@@ -16,7 +17,9 @@ int main(){
     uint64_t q[N][bit_dim + 1];
 	//uint64_t q_cpu[bit_dim + 1], q_hwacha[bit_dim+1];
     for(int z = 0; z < N; z++){
-        q[z] = projM_pos_GSR[z]
+        for (int y=0; y<bit_dim+1; y++){
+            q[z][y] = projM_pos_GSR[z][y];
+        }
     }
     //CPU temporal encoder
     #if PROFILE == 1
@@ -33,7 +36,7 @@ int main(){
     }
     #if PROFILE == 1
         printf("Temporal cycles: %llu\n", read_cycles() - temporal_start);
-        uint64_t temporal_start = read_cycles();
+        temporal_start = read_cycles();
     #endif
     //}
 
@@ -48,7 +51,7 @@ int main(){
 
     for(int b = 0; b < bit_dim+1; ){
         asm volatile ("vsetvl %0, %1" : "=r" (consumed) : "r" (bit_dim+1-b));
-        asm volatsile ("vmca va0, %0" : : "r" (&q[0][b]));
+        asm volatile ("vmca va0, %0" : : "r" (&q[0][b]));
         asm volatile ("vmca va1, %0" : : "r" (&q[1][b]));
         asm volatile ("vmca va2, %0" : : "r" (&q[2][b]));
         asm volatile ("la %0, ngram_bind_v" : "=r" (ngram_bind_addr));
