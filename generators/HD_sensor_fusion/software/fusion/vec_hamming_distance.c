7924530b84248a2b22256e33d4842e4af78edcec
Alisha Menon <allymenon@berkeley.edu>
2020-05-13 16:04:37 -0700

    slowly changing AM

--- generators/HD_sensor_fusion/software/fusion/vec_hamming_distance.c
+++ generators/HD_sensor_fusion/software/fusion/vec_hamming_distance.c
@@ -19,7 +19,7 @@ void vec_hamming_distance(uint64_t q[bit_dim + 1], uint64_t aM[][bit_dim + 1], i
 ***************************************************************************/
 
     int r_tmp = 0;
-    void * AM_addr;
+    void * hamming_addr;
     void * popcount_addr;
     uint64_t tmp[bit_dim + 1];
     int consumed;
@@ -46,7 +46,7 @@ void vec_hamming_distance(uint64_t q[bit_dim + 1], uint64_t aM[][bit_dim + 1], i
             i = tmp[j];
             i = i - ((i >> 1) & 0x5555555555555555);
             i = (i & 0x3333333333333333) + ((i >> 2) & 0x3333333333333333);
-            r_temp += (((i + (i >> 4)) & 0x0F0F0F0F0F0F0F0F) * 0x0101010101010101) >> 56;
+            r_tmp += (((i + (i >> 4)) & 0x0F0F0F0F0F0F0F0F) * 0x0101010101010101) >> 56;
             //asm volatile ("la %0, popcount_v" : "=r" (popcount_addr));
             //asm volatile ("vf 0(%0)" : : "r" (popcount_addr));
         }
