9149953be69873226f60726a0496abfa4d055473
Alisha Menon <allymenon@berkeley.edu>
2020-05-13 15:48:32 -0700

    debugging
    git commit -m debugging

--- generators/HD_sensor_fusion/software/fusion/vec_hamming_distance.c
+++ generators/HD_sensor_fusion/software/fusion/vec_hamming_distance.c
@@ -20,7 +20,7 @@ void vec_hamming_distance(uint64_t q[bit_dim + 1], uint64_t aM[][bit_dim + 1], i
 
     int r_tmp = 0;
     void * AM_addr;
-    uint64_t tmp = 0;
+    uint64_t tmp[bit_dim + 1] = 0;
     int consumed;
     //CHANGE NUMBER OF VECTOR REGISTERS NEEDED
     asm volatile ("vsetcfg %0" : : "r" (VCFG(2, 0, 0, 1)));
@@ -32,15 +32,31 @@ void vec_hamming_distance(uint64_t q[bit_dim + 1], uint64_t aM[][bit_dim + 1], i
             asm volatile ("vsetvl %0, %1" : "=r" (consumed) : "r" (bit_dim+1-j));
             asm volatile ("vmca va0, %0" : : "r" (aM[i][j]));
             asm volatile ("vmca va1, %0" : : "r" (q[j]));
-            asm volatile ("vmca va2, %0" : : "r" (tmp));
+            asm volatile ("vmca va2, %0" : : "r" (tmp[j]));
             asm volatile ("la %0, AM_v" : "=r" (AM_addr));
             asm volatile ("vf 0(%0)" : : "r" (AM_addr));
             //tmp = q[j] ^ aM[i][j];
             j += consumed;
         }
-        r_tmp += numberOfSetBits(tmp);
+        for(int j = 0; j < bit_dim+1; j++){
+            r_tmp += numberOfSetBits(tmp[j]);
+        }
         sims[i] = r_tmp;
         r_tmp = 0;
     }
+
+    //int r_tmp = 0;
+
+    //uint64_t tmp = 0;
+    //for(int i = 0; i < classes; i++){
+    //    for(int j = 0; j < bit_dim + 1; j++){
+
+    //        tmp = q[j] ^ aM[i][j];
+
+    //        r_tmp += numberOfSetBits(tmp);
+    //    }
+    //    sims[i] = r_tmp;
+    //    r_tmp = 0;
+    //}
 }
 
