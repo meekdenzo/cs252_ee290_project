59ddc16f40c82676be022213dea64be29276214b
Alisha Menon <allymenon@berkeley.edu>
2020-05-13 16:15:45 -0700

    constants

--- generators/HD_sensor_fusion/software/fusion/vec_hamming_distance.c
+++ generators/HD_sensor_fusion/software/fusion/vec_hamming_distance.c
@@ -28,8 +28,14 @@ void vec_hamming_distance(uint64_t q[bit_dim + 1], uint64_t aM[][bit_dim + 1], i
     asm volatile ("vsetcfg %0" : : "r" (VCFG(2, 0, 0, 1)));
 
     //constants
-    uint64_t one = 0x1ULL;
-
+    uint64_t const1 = 0x1ULL;
+    uint64_t const2 = 0x5555555555555555;
+    uint64_t const3 = 0x3333333333333333;
+    uint64_t const4 = 0x2ULL;
+    uint64_t const5 = 0x4ULL;
+    uint64_t const6 = 0x0F0F0F0F0F0F0F0F;
+    uint64_t const7 = 0x0101010101010101;
+    uint64_t const8 = 0x38ULL;
     asm volatile ("vmcs vs1, %0" : : "r" (one));
 
 
@@ -44,12 +50,14 @@ void vec_hamming_distance(uint64_t q[bit_dim + 1], uint64_t aM[][bit_dim + 1], i
             j += consumed;
         }
         for(int j = 0; j < bit_dim+1; j++){
+            asm volatile ("vsetvl %0, %1" : "=r" (consumed) : "r" (bit_dim+1-j));
             i = tmp[j];
-            i = i - ((i >> 1) & 0x5555555555555555);
-            i = (i & 0x3333333333333333) + ((i >> 2) & 0x3333333333333333);
-            r_tmp += ((((i + (i >> 4)) & 0x0F0F0F0F0F0F0F0F) * 0x0101010101010101) >> 56);
+            i = i - ((i >> const1) & const2);
+            i = (i & const3) + ((i >> const4) & const3);
+            r_tmp += ((((i + (i >> const5)) & const6) * const7) >> const8);
             //asm volatile ("la %0, popcount_v" : "=r" (popcount_addr));
             //asm volatile ("vf 0(%0)" : : "r" (popcount_addr));
+            j += consumed;
         }
         sims[y] = r_tmp;
         r_tmp = 0;
