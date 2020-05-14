19a0259b9b986594fa1dec7d97773b321d353c0c
Alisha Menon <allymenon@berkeley.edu>
2020-05-14 12:17:36 -0700

    uncached

--- generators/HD_sensor_fusion/software/fusion/temporal.c
+++ generators/HD_sensor_fusion/software/fusion/temporal.c
@@ -16,7 +16,7 @@ int main(){
     //float buffer[channels_EEG];
     uint64_t q[3*N][bit_dim + 1];
 	//uint64_t q_cpu[bit_dim + 1], q_hwacha[bit_dim+1];
-    for(int z = 0; z < N; z++){
+    for(int z = 0; z < 3*N; z++){
         for (int y=0; y<bit_dim+1; y++){
             q[z][y] = projM_pos_GSR[z][y];
         }
@@ -39,11 +39,11 @@ int main(){
     #endif
     //}
 
-    for(int z = N; z < N*2; z++){
-        for (int y=0; y<bit_dim+1; y++){
-            q[z][y] = projM_pos_GSR[z][y];
-        }
-    }
+    //for(int z = N; z < N*2; z++){
+    //    for (int y=0; y<bit_dim+1; y++){
+    //        q[z][y] = projM_pos_GSR[z][y];
+    //    }
+    //}
     //CPU bit shift temporal encoder
     #if PROFILE == 1
         temporal_start = read_cycles();
@@ -64,11 +64,11 @@ int main(){
     //}
 
     //Hwacha temporal encoder
-    for(int z = 2*N; z < N*3; z++){
-        for (int y=0; y<bit_dim+1; y++){
-            q[z][y] = projM_pos_GSR[z][y];
-        }
-    }
+    //for(int z = 2*N; z < N*3; z++){
+    //    for (int y=0; y<bit_dim+1; y++){
+    //        q[z][y] = projM_pos_GSR[z][y];
+    //    }
+    //}
 
     //temporal encode hardcoded for N=3
     void * ngram_bind_addr;
