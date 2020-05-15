4da9b400183bb04f1f350f116899c9d89a652a80
Alisha Menon <allymenon@berkeley.edu>
2020-05-14 17:17:10 -0700

    reset

--- generators/HD_sensor_fusion/software/fusion/main_vec.c
+++ generators/HD_sensor_fusion/software/fusion/main_vec.c
@@ -58,7 +58,7 @@ int main(){
         printf("Initial Spatial cycles: %llu\n", read_cycles() - spatial_start);
     #endif
 
-	for(int ix = 0; ix < 30-N+1; ix++){
+	for(int ix = 0; ix < 5-N+1; ix++){
 
         #if PROFILE == 1
             uint64_t temporal_start = read_cycles();
@@ -105,7 +105,7 @@ int main(){
  	        printf("Sample %d (predicted, golden) class: (%d, %d)\n", ix, class, labels[ix]);
         #endif
 
-        if (ix < 30-N) {
+        if (ix < 5-N) {
             //Move forward by updating q and spatially encoding ix+Nth sample
             #if PROFILE == 1
                 spatial_start = read_cycles();
